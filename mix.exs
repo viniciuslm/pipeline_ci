1e64f0dd0dac00b7c08e6a1607f7bd44d864bc29
Vinicius Moreira <viniciuslm@gmail.com>
2022-03-02 21:09:48 -0300

    fix mix format

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule PipelineCi.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test,
+        "coveralls.html": :test
       ]
     ]
   end
